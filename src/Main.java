ad5624534c4f81e0c729248ff5cab4592345eb4f
mdriadmahamudsirazy <riadmahamud8@gmail.co,m>
2024-03-13 10:08:41 -0400

    Final Push

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String X = "Riyadh";
+        String Y = "Sirazy";
+        System.out.println("Hello I am " + X + " " + Y);
+        System.out.println("I am Enrolled in Java Training");
+    }
+}
